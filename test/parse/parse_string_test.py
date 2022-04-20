b13aff3ea21bc4e9c039cf252ed5a4efacda82f1
Eric Erkela <eerkela42@gmail.com>
2022-04-20 13:47:16 -0700

    set up parse test directory

---
+++ test/parse/parse_string_test.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from datetime import datetime, timedelta, timezone
+import random
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from context import pdtypes
+import pdtypes.parse
+
+
+random.seed(12345)
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
